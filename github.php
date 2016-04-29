53740e9c4706e8a9753ae047b6faa282537b9377
shawnqiang <shawndotty@gmail.com>
2016-04-29 15:57:44 -0400

    change github pull

--- github.php
+++ github.php
@@ -1,1 +1,15 @@
-<?php `git pull`;
+<?php
+
+if ( $_POST['payload'] ) {
+
+
+    // If there is already a repo, just run a git pull to grab the latest changes
+    shell_exec("git pull");
+
+    die("done " . mktime());
+
+} else  {
+    
+    die("Sorry this is not right");
+}
+
